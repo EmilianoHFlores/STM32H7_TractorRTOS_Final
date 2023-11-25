01fb26bbf6070d5a35c4200091b6d2bb38be36b1
EmilianoHFlores <A00833160@tec.mx>
2023-11-24 22:02:04 -0600

    led changes

--- CM7/Core/Inc/main.h
+++ CM7/Core/Inc/main.h
@@ -67,10 +67,10 @@ void Error_Handler(void);
 #define LD1_GPIO_Port GPIOB
 #define LD3_Pin GPIO_PIN_14
 #define LD3_GPIO_Port GPIOB
-#define Left_Blinker_Pin GPIO_PIN_8
-#define Left_Blinker_GPIO_Port GPIOB
-#define Right_Blinker_Pin GPIO_PIN_9
-#define Right_Blinker_GPIO_Port GPIOB
+#define Right_Blinker_Pin GPIO_PIN_6
+#define Right_Blinker_GPIO_Port GPIOD
+#define Left_Blinker_Pin GPIO_PIN_7
+#define Left_Blinker_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
